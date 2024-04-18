4010f850ba8a4200e684d37a5dda4fec0011c127
juliang2127 <164977539+juliang2127@users.noreply.github.com>
2024-04-18 03:57:47 +0000

    getting there

--- app.rb
+++ app.rb
@@ -1,9 +1,39 @@
 require "sinatra"
 require "sinatra/reloader"
+require "json"
+require "http"
+
+api_url = "https://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATE_KEY"]}"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  api_url = "https://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATE_KEY"]}"
+
+  # use HTTP.get to retrieve the API information
+  raw_data = HTTP.get(api_url)
+
+  # convert the raw request to a string
+  raw_data_string = raw_data.to_s
+
+  # convert the string to JSON
+  @parsed_data = JSON.parse(raw_data_string)
+
+  @currency_parsed = @parsed_data.fetch("currencies",{})
+
+  @currency_parsed_keys = @currency_parsed.keys
+
+  #currency_parsed_keys.each do |zebra, giraffe|
+    #pp "Key: #{zebra}"
+   # pp "Value: #{giraffe}"
+  #end
+
+
+
+
+  # Here, get the currency symbols from the JSON, I recommend starting by 
+  # making it an instance variable (@parsed_data), then showing that 
+  # instance variable in the `views/homepage.erb` view template that you 
+  # are rendering below (use <%= %> ERB tags to render it in that view template).
+
+  # render a view template where I show the symbols
+  erb(:homepage)
 end
