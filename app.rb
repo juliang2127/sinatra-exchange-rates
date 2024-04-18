db5222e9e0fd1b50bce16a4a6a112ac6a269dec6
juliang2127 <164977539+juliang2127@users.noreply.github.com>
2024-04-18 15:52:52 +0000

    exchange done

--- app.rb
+++ app.rb
@@ -21,14 +21,6 @@ get("/") do
 
   @currency_parsed_keys = @currency_parsed.keys
 
-  #currency_parsed_keys.each do |zebra, giraffe|
-    #pp "Key: #{zebra}"
-   # pp "Value: #{giraffe}"
-  #end
-
-
-
-
   # Here, get the currency symbols from the JSON, I recommend starting by 
   # making it an instance variable (@parsed_data), then showing that 
   # instance variable in the `views/homepage.erb` view template that you 
@@ -36,4 +28,47 @@ get("/") do
 
   # render a view template where I show the symbols
   erb(:homepage)
+end
+
+get("/:from_currency") do
+  @original_currency = params.fetch("from_currency")
+
+  api_url = "https://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATE_KEY"]}"
+
+  raw_data = HTTP.get(api_url)
+
+  # convert the raw request to a string
+  raw_data_string = raw_data.to_s
+
+  # convert the string to JSON
+  @parsed_data = JSON.parse(raw_data_string)
+
+  @currency_parsed = @parsed_data.fetch("currencies",{})
+
+  @currency_parsed_keys = @currency_parsed.keys
+  
+  # some more code to parse the URL and render a view template
+  # (HINT: this code is identical to the first route, you just
+  # render a different view template at the end.)
+  erb(:flexible)
+end
+
+get("/:from_currency/:to_currency") do
+  @original_currency = params.fetch("from_currency")
+  @destination_currency = params.fetch("to_currency")
+
+  api_url = "https://api.exchangerate.host/convert?access_key=#{ENV["EXCHANGE_RATE_KEY"]}&from=#{@original_currency}&to=#{@destination_currency}&amount=1"
+
+  raw_data = HTTP.get(api_url)
+
+  # convert the raw request to a string
+  raw_data_string = raw_data.to_s
+
+  # convert the string to JSON
+  @parsed_data = JSON.parse(raw_data_string)
+
+  @parsed_data_info = @parsed_data.fetch("result")
+
+  # some more code to parse the URL and render a view template
+  erb(:flexible2)
 end
